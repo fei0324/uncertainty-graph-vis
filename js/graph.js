22c9b62e69ae71ea05a2f42888ea7a9b9361a2de
mkcyoung <m.c.young0@gmail.com>
2020-03-13 23:44:32 -0600

    created average scale and mapped to edges

--- js/graph.js
+++ js/graph.js
@@ -15,16 +15,17 @@ class Graph{
         // Creating scales
 
         //finding max and min of mean
-        // let eg_maxR = d3.max(this.gapData.map( d=> d.r_eg));
-        // let eg_maxD = d3.max(this.gapData.map( d=> d.d_eg));
+        let avg_array = this.data.links.map( d=> d.average)
+        let maxMean = d3.max(avg_array);
+        let minMean = d3.min(avg_array);
+        let medMean = d3.median(avg_array)
+        console.log("max:",maxMean,"min:",minMean,"median:",medMean)
 
-
-        // //Color scale on diverging red blue axis
-        // this.color = d3.scaleDiverging([-eg_maxR, 0, eg_maxD], d3.interpolateRdBu);
-
-        // //Color scale for legislative effectiveness
-        // this.color_le = d3.scaleSequential(d3.interpolatePlasma).domain([le_min,3]);
-         
+        //Color scale for means
+        //Experimenting by making the median the diverging point, if this doesn't work, could change to mean
+        //this.color = d3.scaleDiverging([minMean, medMean, maxMean], d3.interpolateRdBu);
+        this.color = d3.scaleSequential(d3.interpolatePlasma).domain([minMean,maxMean]);
+        // console.log(d3.color(this.color(10)).formatHex())
     
     }
 
@@ -43,122 +44,126 @@ class Graph{
 
         // Graph with links that have a width/color based on mean 
         myGraph(document.getElementById('graph'))
-        .graphData(data)
-        .nodeRelSize(2)
-        .nodeColor(() => "black")
-        .nodeLabel(node => node.id)
-        .linkCanvasObjectMode(() => 'replace')
-        .linkCanvasObject((link, ctx) => {
+            .graphData(data)
+            .nodeRelSize(2)
+            .nodeColor(() => "black")
+            .nodeLabel(node => node.id)
+            .linkCanvasObjectMode(() => 'replace')
+            .linkCanvasObject((link, ctx) => {
+                // This draws the links' uncertainty viz
 
-            // Path node margin is how far out the edge uncertainty starts to taper
-            const PATH_NODE_MARGIN = 5;
-            // start and end coordinates
-            const start = link.source;
-            const end = link.target;
+                // Path node margin is how far out the edge uncertainty starts to taper
+                const PATH_NODE_MARGIN = 5;
+                // start and end coordinates
+                const start = link.source;
+                const end = link.target;
 
-            // ignore unbound links
-            if (typeof start !== 'object' || typeof end !== 'object') return;
+                // ignore unbound links
+                if (typeof start !== 'object' || typeof end !== 'object') return;
 
-            // calculate path midpoint
-            const midPos = Object.assign(...['x', 'y'].map(c => ({
-                [c]: start[c] + (end[c] - start[c]) / 2 // calc middle point
-            })));
-            //console.log(midPos.x)
+                // calculate path midpoint
+                const midPos = Object.assign(...['x', 'y'].map(c => ({
+                    [c]: start[c] + (end[c] - start[c]) / 2 // calc middle point
+                })));
+                //console.log(midPos.x)
 
-            // Relative positioning along line
-            const relLink = { x: end.x - start.x, y: end.y - start.y };
+                // Relative positioning along line
+                const relLink = { x: end.x - start.x, y: end.y - start.y };
 
-            // I need to calculate the positiong of the 2 outer-most path points
-            let textAngle = Math.atan2(relLink.y, relLink.x);
-            // Edge angle is separate to interpolate inner edge points
-            let edgeAngle = textAngle;
+                // I need to calculate the positiong of the 2 outer-most path points
+                let textAngle = Math.atan2(relLink.y, relLink.x);
+                // Edge angle is separate to interpolate inner edge points
+                let edgeAngle = textAngle;
 
-            // Maintains orientation
-            if (textAngle > 0) textAngle = (Math.PI - textAngle);
-            if (textAngle < 0) textAngle = (-Math.PI - textAngle);
+                // Maintains orientation
+                if (textAngle > 0) textAngle = (Math.PI - textAngle);
+                if (textAngle < 0) textAngle = (-Math.PI - textAngle);
 
-            const scaling = 1
-            const mean_val = link.average * scaling;
-            const std_val = link.standard_deviation * scaling;
-            let x_prime = Math.sin(textAngle)*mean_val;
-            let y_prime = Math.cos(textAngle)*mean_val;
-            let xs_prime = Math.sin(textAngle)*(mean_val+std_val);
-            let ys_prime = Math.cos(textAngle)*(mean_val+std_val);
+                const scaling = 1
+                const mean_val = link.average * scaling;
+                const std_val = link.standard_deviation * scaling;
+                let x_prime = Math.sin(textAngle)*mean_val;
+                let y_prime = Math.cos(textAngle)*mean_val;
+                let xs_prime = Math.sin(textAngle)*(mean_val+std_val);
+                let ys_prime = Math.cos(textAngle)*(mean_val+std_val);
 
-            // Calculating points that are in from the edges
-            let xe_prime = Math.cos(edgeAngle)*PATH_NODE_MARGIN;
-            let ye_prime = Math.sin(edgeAngle)*PATH_NODE_MARGIN;
+                // Calculating points that are in from the edges
+                let xe_prime = Math.cos(edgeAngle)*PATH_NODE_MARGIN;
+                let ye_prime = Math.sin(edgeAngle)*PATH_NODE_MARGIN;
 
-            //line data for the mean shape
-            let mean_data = [
-                [start.x , start.y ], // point at the end
-                [start.x + xe_prime, start.y + ye_prime], // point a certain distance in from the end
-                [midPos.x + x_prime , midPos.y + y_prime ], // point orthogonal from midpoint
-                [end.x - xe_prime, end.y - ye_prime], // point a certaing distance in from other end 
-                [ end.x , end.y ], // point at other end
-                [end.x - xe_prime, end.y - ye_prime], // point a certaing distance in from other end 
-                [ midPos.x - x_prime , midPos.y - y_prime ], // other point orthogonal from midpoint
-                [start.x + xe_prime, start.y + ye_prime], // point a certain distance in from the end
-                [start.x , start.y ] // back to start
-            ]
+                //line data for the mean shape
+                let mean_data = [
+                    [start.x , start.y ], // point at the end
+                    [start.x + xe_prime, start.y + ye_prime], // point a certain distance in from the end
+                    [midPos.x + x_prime , midPos.y + y_prime ], // point orthogonal from midpoint
+                    [end.x - xe_prime, end.y - ye_prime], // point a certaing distance in from other end 
+                    [ end.x , end.y ], // point at other end
+                    [end.x - xe_prime, end.y - ye_prime], // point a certaing distance in from other end 
+                    [ midPos.x - x_prime , midPos.y - y_prime ], // other point orthogonal from midpoint
+                    [start.x + xe_prime, start.y + ye_prime], // point a certain distance in from the end
+                    [start.x , start.y ] // back to start
+                ]
 
-            let std_data = [
-                [start.x , start.y ], // point near the end 
-                [start.x + xe_prime, start.y + ye_prime], // point a certain distance in from the end
-                [midPos.x + xs_prime , midPos.y + ys_prime ], // point orthogonal from midpoint
-                [end.x - xe_prime, end.y - ye_prime], // point a certaing distance in from other end 
-                [ end.x , end.y ], // other point near the end
-                [end.x - xe_prime, end.y - ye_prime], // point a certaing distance in from other end 
-                [ midPos.x - xs_prime , midPos.y - ys_prime ], // other point orthogonal from midpoint
-                [start.x + xe_prime, start.y + ye_prime], // point a certain distance in from the end
-                [start.x , start.y ] // back to start
-            ]
+                let std_data = [
+                    [start.x , start.y ], // point near the end 
+                    [start.x + xe_prime, start.y + ye_prime], // point a certain distance in from the end
+                    [midPos.x + xs_prime , midPos.y + ys_prime ], // point orthogonal from midpoint
+                    [end.x - xe_prime, end.y - ye_prime], // point a certaing distance in from other end 
+                    [ end.x , end.y ], // other point near the end
+                    [end.x - xe_prime, end.y - ye_prime], // point a certaing distance in from other end 
+                    [ midPos.x - xs_prime , midPos.y - ys_prime ], // other point orthogonal from midpoint
+                    [start.x + xe_prime, start.y + ye_prime], // point a certain distance in from the end
+                    [start.x , start.y ] // back to start
+                ]
+
+                // draw edge uncertainties
+                ctx.save();
+
+                //Retrieve color and adjust opacity
+                let colorM = d3.color(this.color(link.average)).copy({opacity: 0.7})
+                let colorStd = d3.color(this.color(link.average)).copy({opacity: 0.45});
+                // can also explore # color.brighter([k]) <> https://github.com/d3/d3-color for std instead of opacity
+
+                //Line for mean
+                const line = d3.line()
+                    .curve(d3.curveBasisClosed); //good one
+                // .curve(d3.curveCardinalClosed.tension(1)); //adjusting tension is cool
+                    //.curve(d3.curveCatmullRomClosed.alpha(0.5));
+                line.context(ctx); // for canvas
+                ctx.beginPath();
+                line(mean_data);
+                ctx.lineWidth = 0.1
+                ctx.fillStyle = colorM 
+                //Shadow effect 
+                // ctx.shadowColor = 'rgba(46, 213, 197, 0.6)';
+                // ctx.shadowBlur = 100;
+                ctx.fill();
+                //ctx.stroke();
+
+                //Line for first std dev
+                const line2 = d3.line()
+                    .curve(d3.curveBasisClosed); //good one
+                    //.curve(d3.curveCardinalClosed.tension(0)); //adjusting tension is cool
+                    //.curve(d3.curveCatmullRomClosed.alpha(0.5));
+                line2.context(ctx); // for canvas
+                ctx.beginPath();
+                line(std_data);
+                ctx.lineWidth = 0.1
+                ctx.fillStyle = colorStd;
+                //Shadow effect 
+                // ctx.shadowColor = 'red';
+                // ctx.shadowBlur = 100;
+                ctx.fill();
+                //ctx.stroke();
+
+                ctx.restore();
 
 
-            // draw edge uncertainties
-            ctx.save();
 
-
-            //Line for mean
-            const line = d3.line()
-                .curve(d3.curveBasisClosed); //good one
-            // .curve(d3.curveCardinalClosed.tension(1)); //adjusting tension is cool
-                //.curve(d3.curveCatmullRomClosed.alpha(0.5));
-            line.context(ctx); // for canvas
-            ctx.beginPath();
-            line(mean_data);
-            ctx.lineWidth = 0.1
-            ctx.fillStyle = 'rgba(46, 213, 197, 0.6)'; //This will eventually be chosen via d3.scale
-            //Shadow effect 
-            // ctx.shadowColor = 'rgba(46, 213, 197, 0.6)';
-            // ctx.shadowBlur = 100;
-            ctx.fill();
-            //ctx.stroke();
-
-            //Line for first std dev
-            const line2 = d3.line()
-                .curve(d3.curveBasisClosed); //good one
-                //.curve(d3.curveCardinalClosed.tension(0)); //adjusting tension is cool
-                //.curve(d3.curveCatmullRomClosed.alpha(0.5));
-            line2.context(ctx); // for canvas
-            ctx.beginPath();
-            line(std_data);
-            ctx.lineWidth = 0.1
-            ctx.fillStyle = 'rgba(46, 213, 197, 0.4)';
-            //Shadow effect 
-            // ctx.shadowColor = 'red';
-            // ctx.shadowBlur = 100;
-            ctx.fill();
-            //ctx.stroke();
-
-            ctx.restore();
-
-
-
-        })
-        // .linkColor(() => "#4E4E54")
-        // .linkWidth(link => link.average)
-        .zoom(1.5);
+            })
+            // .linkColor(() => "#4E4E54")
+            // .linkWidth(link => link.average)
+            .zoom(1.5);
 
 
     }
